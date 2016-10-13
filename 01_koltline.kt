ca297a65dfc671eef7339316430ae71472227148
maxiaoqian <sandy199202@qq.com>
2016-10-13 15:51:54 +0800

    Update 01_koltline.kt

--- 01_koltline.kt
+++ 01_koltline.kt
@@ -1,3 +1,17 @@
+package com.kotlin_learn.hello_world
+
+/**
+dehudhe
+dejide
+*/
+
+func hello(guest: String): String = "hello, " + guest + "!"
+
 func main(args:Array<String>){
     println("hello world")
-    }
+    println(hello("ccc"))
+    println("""
+        multi
+        line
+        """  )
+  }
